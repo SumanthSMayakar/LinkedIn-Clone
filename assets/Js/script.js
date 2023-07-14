a664669e20f4b91536eca356e92cb05c6c3f3f97
SumanthSMayakar <sumanthmayakar@gmail.com>
2023-07-14 18:25:16 +0530

    LinkedIn-clone

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -3,3 +3,16 @@ let profileMenu = document.getElementById('profileMenu')
 function toggleMenu(){
     profileMenu.classList.toggle('open-menu');
 }
+
+let sideActivity = document.getElementById('sidebarActivity');
+let moreLink = document.getElementById('showMoreLink');
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
+
+    if(sideActivity.classList.contains('open-activity')){
+        moreLink.innerHTML = "Show Less <b>-</b>";
+    }else{
+        moreLink.innerHTML = "Show More <b>+</b>";
+    }
+}
